2cbb7f22e32d3adbdcdd34ac4c3e8535d919ad8d
Muhammad Asad Kamran <asad@kamilinx.com>
2019-03-24 13:41:47 +0500

    Till component with events

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
-@Component({
+import { Recipe } from '../recipe.model';
+ @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[]=[
+    new Recipe('A Test Recipe','This is a sample recipe',
+    'https://norecipes.com/wp-content/uploads/2018/08/teriyaki-chicken-recipe_010.jpg'),
+    new Recipe('Another Recipe','Another sample recipe',
+    'https://norecipes.com/wp-content/uploads/2018/08/teriyaki-chicken-recipe_010.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
